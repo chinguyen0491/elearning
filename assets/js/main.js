123556d49afce4b0f5be35802d2ee4e767623f9e
NguyenHai <haicoi201@gmail.com>
2022-03-19 12:19:28 +0700

    update handle BTN TO TOP

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,22 @@
 // ==================== HANDLE HEADER SECTION ==================== //
 const headerSection = document.querySelector('.header__section')
+const btnToTop = document.querySelector('.btn__toTop')
+let prevScroll
 
 window.onscroll = () => {
-    if(window.scrollY > 120) {
+    const st = window.scrollY
+    if(st > 120) {
         headerSection.classList.add('active')
     } else {
         headerSection.classList.remove('active')
     }
+
+    if(st < prevScroll && st > 200) {
+        console.log('up')
+        btnToTop.classList.add('active')
+    } else {
+        console.log('down')
+        btnToTop.classList.remove('active')
+    }
+    prevScroll = st
 }
